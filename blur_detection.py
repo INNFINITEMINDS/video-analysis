07e79872a8ea5275b6c2a1daa83d3be7066601a1
keresmi <>
2018-03-04 21:07:26 +0100

    --show argument added

--- blur_detection.py
+++ blur_detection.py
@@ -34,11 +34,15 @@ def quality_mark(fm):
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", required=True, help="path to input video")
 ap.add_argument("-o", "--output", required=True, help="path to video processing results")
+ap.add_argument("-s", "--show", action="store_true", help="show video with results")
 args = vars(ap.parse_args())
 
 print("[INFO] Starting video processing...")
 fvs = FileVideoStream(args["video"]).start()
 time.sleep(1.0)
+
+total_frames = int(fvs.stream.get(cv2.CAP_PROP_FRAME_COUNT))
+fm_sum = 0
 
 file = open(args["output"], 'wb')
 file.write('Timestamp, Variance of Laplacian, Quality Mark')
@@ -51,24 +55,27 @@ while(fvs.more()):
     frame = fvs.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     fm = variance_of_laplacian(gray)
+    fm_sum += fm
     timestamp = fvs.stream.get(cv2.CAP_PROP_POS_MSEC)
 
     # save results to file
     file.write('{0}, {1}, {2}'.format(timestamp, fm, quality_mark(fm)))
     file.write('\n')
 
-    # display results
-    frame = imutils.resize(frame, width=450)
-    cv2.putText(frame, "{:.2f}".format(fm), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-    cv2.rectangle(frame, (0, 1080), (int(fm*1.6), 1040), (0, 0, 255), thickness=cv2.FILLED)
+    # show the frame and the result
+    if args["show"]:
+        frame = imutils.resize(frame, width=450)
+        cv2.putText(frame, "{:.2f}".format(fm), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+        cv2.rectangle(frame, (0, 1080), (int(fm*1.6), 1040), (0, 0, 255), thickness=cv2.FILLED)
+        cv2.imshow("Video", frame)
 
-    # show the frame and update the FPS counter
-    cv2.imshow("Video", frame)
+    # update the FPS counter
     cv2.waitKey(1)
     fps.update()
 
 # stop the timer and display FPS information
 fps.stop()
+print("[INFO] Avg. variance of Laplacian: {:.2f}".format(fm_sum/total_frames))
 print("[INFO] Elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] Approx. FPS: {:.2f}".format(fps.fps()))
 
